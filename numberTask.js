0696757fb3f7d8c87088437d6e449afc0ba1463b
it-AVNG <it.avng@gmail.com>
2022-10-31 22:50:56 +0700

    update Number lesson

---
+++ numberTask.js
@@ -0,0 +1,17 @@
+let x = 1;
+let y= 3;
+let s ='my' + ' string';
+alert(s);
+
+// alert( x ) ;
+// alert ( y - x );
+
+// alert (5 % 2);
+// alert (8 % 3);
+
+// alert( 2**2 ); 
+// alert( 2**3 );
+// alert( 2**4 );
+
+// alert( 2** (1/2))
+// alert(2** (3/4))
